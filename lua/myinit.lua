54e5efafc8748f3081ba4bcf33d2b2c9d2baa192
Bertrand Darbon <bertrand.darbon@ledger.fr>
2024-05-21 14:37:34 +0200

    🎨 format

--- lua/myinit.lua
+++ lua/myinit.lua
@@ -4,15 +4,16 @@ vim.opt.title = true
 
 vim.opt.clipboard = "unnamedplus"
 
-api.nvim_create_autocmd("BufEnter", { pattern='*', command = 'let &titlestring = system(\'pwd | sed "s#.*/##"\') .. " - " .. expand("%:t")'})
+api.nvim_create_autocmd("BufEnter",
+  { pattern = '*', command = 'let &titlestring = system(\'pwd | sed "s#.*/##"\') .. " - " .. expand("%:t")' })
 vim.api.nvim_create_autocmd("BufRead", {
-    group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
-    pattern = "Cargo.toml",
-    callback = function()
-        require("cmp").setup.buffer({ sources = { { name = "crates" } } })
-    end,
+  group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+  pattern = "Cargo.toml",
+  callback = function()
+    require("cmp").setup.buffer({ sources = { { name = "crates" } } })
+  end,
 })
 
-vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
-vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
+vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
 vim.opt.conceallevel = 1
