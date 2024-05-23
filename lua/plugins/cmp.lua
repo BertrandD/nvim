4c561986e8d23a5e0549fd21c48674281391af52
Bertrand Darbon <bertrand.darbon@ledger.fr>
2024-05-23 17:46:12 +0200

    ✨ Add copilot

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    opts = function()
+      local merge_tb = vim.tbl_deep_extend
+      local defaults = require "nvchad.configs.cmp"
+
+      local opts = {
+        sources = {
+          { name = "copilot" },
+          -- other sources from nvchad
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "nvim_lua" },
+          { name = "path" },
+        }
+      }
+      -- return defaults
+      return merge_tb("force", defaults, opts)
+    end,
+  }
+}
